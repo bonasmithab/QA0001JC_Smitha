4b2945db56fd2f6d2452799671346dcf83628829
“Smitha” <bonasmitha.behura@gmail.com>
2024-05-15 12:04:46 +0200

    ArraysAndMultidimensionalArrays

---
+++ src/test/java/Excercises/LoopsTest01.java
@@ -0,0 +1,11 @@
+package Excercises;
+
+import java.util.Arrays;
+
+public class LoopsTest01 {
+    public static void main(String[] args) {
+        String s = "Java, I like Java?";
+        String arr[] = s.split("a");
+        System.out.println("The number of the character: " + (arr.length));
+    }
+}
